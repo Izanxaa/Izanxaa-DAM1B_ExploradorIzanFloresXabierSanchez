80692adbe0dea8b4f339c3d3cdf2a8e51443e224
Izan <ifloressan@educacion.navarra.es>
2025-01-07 15:21:54 +0100

    metodo moverse en la clase enemigo usando arrays

--- DAM1B_ExploradorIzanFlores/src/Enemigo.java
+++ DAM1B_ExploradorIzanFlores/src/Enemigo.java
@@ -28,7 +28,26 @@ import java.util.Random;
         // Metodo moverse
 
         public void moverse (int direccion) {
+            /*En este caso he decidido hacer la funcion con un array, el primero es arriba por lo tanto debemos restarle
+            uno a la fila, el siguiente es abajo por lo que sumamos una fila, el siguiente es derecha por lo que sumamos
+            una columna y el ultimo es izquierda por lo que restamos una columna*/
+            int [][] movimientos = {{0, -1}, {0, 1}, {1, 0}, {-1, 0}};
 
+            // Comprobamos que la direccion este entre nuestras opciones
+            if (direccion >= 1 && direccion <= 4) {
+                int modificadoX = movimientos[direccion -1][0];
+                int modificadoY = movimientos[direccion -1][1];
+
+                int nuevaX = posicionActual.getCoordenadaFila() + modificadoX;
+                int nuevaY = posicionActual.getCoordenadaCol() + modificadoY;
+
+                // verificamos que no nos salgamos de los limites del mapa
+
+                if (nuevaX >= 0 && nuevaX < 20 && nuevaY >= 0 && nuevaY < 6) {
+                    posicionActual.setCoordenadaFila(nuevaX);
+                    posicionActual.setCoordenadaCol(nuevaY);
+                }
+            }
         }
 
     }
